70dd7a401724c735c0c3ce10ef130d3c28912f75
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-07-15 16:09:49 +0900

    [Silver V] Title: 단어 정렬, Time: 92 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -4,16 +4,9 @@ n = int(sys.stdin.readline())
 words = []
 for _ in range(n):
     words.append(sys.stdin.readline().strip()) #sys롤 문자열을 읽어들일때 꼭 strip해줘야함
-    
+
+words = list(set(words))    
 words.sort(key=lambda x: (len(x), x))
-i = 0
-for _ in range(n):
-    if i > len(words) - 2:
-        break
-    if words[i] in words[i+1:]:
-        words.pop(i)
-        continue
-    i += 1
-    
+
 for item in words:
     print(item)
