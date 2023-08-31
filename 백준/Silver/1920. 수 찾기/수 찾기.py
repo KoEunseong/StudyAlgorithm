b5148e314655cbfc5a70adc89a005950dc60b0c9
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-08-31 13:11:55 +0900

    [Silver IV] Title: 수 찾기, Time: 424 ms, Memory: 47268 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,33 @@
 import sys
-def binary_Search(data_list, value):
-    
+
+N = int(input())
+data = list(map(int , sys.stdin.readline().split()))
+M = int(input())
+finding_data = list(map(int, sys.stdin.readline().split()))
+
+data.sort()
+
+def BinarySearch(num):
     left = 0
-    right = len(data_list) - 1
+    right = len(data) - 1
     
-    while True:
+    while left <= right :
         mid = (left + right) // 2
-        if left > right :
-            return 0
-        elif data_list[mid] == value:
-            return 1
-        elif data_list[mid] > value:
-            right = mid -1
-        elif data_list[mid] < value:
-            left = mid + 1 
+        
+        if num == data[mid] :
+            return True
+        
+        elif num > data[mid] :
+            left = mid + 1
+        else :
+            right = mid - 1
+    
+    return False
 
-
-N = int(sys.stdin.readline())
-A = list(map(int, sys.stdin.readline().split()))
-M = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
-
-A.sort()
-mid = N //2
-for i in range(M):
-    print(binary_Search(A,data[i]))
+for item in finding_data:
+    if BinarySearch(item):
+        print(1)
+    else:
+        print(0)
+            
+            
