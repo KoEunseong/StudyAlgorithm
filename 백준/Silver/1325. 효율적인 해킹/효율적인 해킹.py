1cd57289db5201ebcce245ab0d42e0ad89a5b42e
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-09-15 02:41:39 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10840 ms, Memory: 215460 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,34 +1,59 @@
 import sys
 from collections import deque
 
+
 N, M = map(int, sys.stdin.readline().split())
 data = [[] for i in range(N + 1)]
 
 for _ in range(M):
     n1, n2 = map(int , sys.stdin.readline().split())
-    data[n1].append(n2)
+    data[n2].append(n1)
     
-visited = [0 for _ in range(N + 1)]    
+# print(data)
+# 아 이제 깨달았다1!!!!!!
+
+visited = [False for _ in range(N)]
 
 def BFS(X):
-    
+    visited = [False for _ in range(N + 1)]
+    answer = 0
     myque = deque()
+    visited[X] = True
     myque.append(X)
-    visit[X] = True
+    
     while myque :
         num = myque.popleft()
-        
         for item in data[num]:
-            if not visit[item] :
-                visit[item] = True
-                visited[item] = visited[item] + 1
+            if not visited[item]:
+                visited[item] = True
+                answer += 1
                 myque.append(item)
 
-for i in range(1, N + 1):
-    visit = [False for _ in range(N + 1)]    
-    BFS(i)
+    return answer
 
-max_data = max(visited)
+answer = 0
+answer_list = []
 for i in range(1, N + 1):
-    if visited[i] == max_data :
-        print(i, end=' ')
+    # visited = [False for _ in range(N + 1)]
+    count = BFS(i)
+    # print(count)
+    if count > answer:
+        answer = count
+        answer_list = [i]
+    elif count == answer :
+        answer_list.append(i)
+print(*answer_list)
+
+
+# visited = [False for _ in range(N + 1)]
+# BFS(answer[1])
+# print(answer)
+# print(visited)
+
+# print(visited)
+# for item in visited:
+#     if item :
+#         print(item)
+    
+                    
+    
