7b93b5c05915a00f756d95d4f957a9f530262919
KoEunseong <98599867+KoEunseong@users.noreply.github.com>
2023-10-09 00:16:02 +0900

    [Silver III] Title: 다음 순열, Time: 52 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -0,0 +1,37 @@
+import sys
+
+N = int(input())
+data = list(map(int , sys.stdin.readline().split()))
+
+
+n1 = 0
+for i in range(1, len(data)):
+    if data[i - 1] < data[i]:
+        n1 = i
+
+n2 = 0
+for j in range(n1, len(data)):
+    if data[j] > data[n1 - 1]:
+        n2 = j
+     
+data[n1 - 1], data[n2] = data[n2], data[n1 - 1]
+
+reversedata = list(reversed([item for item in data[n1:]]))
+
+if n1 != 0:
+
+    print(*(data[:n1] + reversedata))
+else :
+    print(-1)
+
+
+
+
+
+
+# point = len(reversedata) - 1
+# for i in range(len(reversedata) // 2):
+#     reversedata[i], reversedata[point] = reversedata[point],reversedata[i]
+#     point -= 1
+
+
